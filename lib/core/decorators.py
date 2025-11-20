fa8df9dcd96b6ae1f7428eebccaf8b60cb5c2f24
codervibe <3217277135@qq.com>
2025-11-21 00:14:41 +0800

    refactor(core): 重构装饰器模块以提升可读性和功能性
    - 移除文件头部的GPL许可证声明
    -为cached装饰器添加详细的中文文档字符串- 改进cached装饰器内部逻辑注释，增强代码可读性- 在cached装饰器中明确缓存键值生成方式
    - 为locked装饰器增加完整的函数和参数说明
    - 添加locked装饰器的执行流程注释- 整体优化装饰器模块的结构与注释规范

--- lib/core/decorators.py
+++ lib/core/decorators.py
@@ -1,21 +1,3 @@
-# -*- coding: utf-8 -*-
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#
-#  Author: Mauro Soria
-
 import threading
 
 from functools import wraps
@@ -27,19 +9,49 @@ _cache_lock = threading.Lock()
 
 
 def cached(timeout=100):
+    """
+    缓存装饰器工厂函数，用于创建带有缓存功能的装饰器
+
+    参数:
+        timeout (int): 缓存超时时间，单位为秒，默认为100秒
+
+    返回:
+        function: 返回一个装饰器函数
+    """
     def _cached(func):
+        """
+        实际的缓存装饰器函数
+
+        参数:
+            func (function): 被装饰的函数
+
+        返回:
+            function: 返回包装后的函数
+        """
         @wraps(func)
         def with_caching(*args, **kwargs):
+            """
+            带有缓存功能的包装函数
+
+            参数:
+                *args: 函数的位置参数
+                **kwargs: 函数的关键字参数
+
+            返回:
+                any: 函数执行结果或缓存的结果
+            """
+            # 生成缓存键值，基于函数ID和参数ID
             key = id(func)
             for arg in args:
                 key += id(arg)
             for k, v in kwargs:
                 key += id(k) + id(v)
 
-            # If it was cached and the cache timeout hasn't been reached
+            # 检查缓存是否存在且未超时
             if key in _cache and time() - _cache[key][0] < timeout:
                 return _cache[key][1]
 
+            # 执行函数并更新缓存
             with _cache_lock:
                 result = func(*args, **kwargs)
                 _cache[key] = (time(), result)
@@ -52,8 +64,29 @@ def cached(timeout=100):
 
 
 def locked(func):
+    """
+    线程锁装饰器，用于确保函数在多线程环境下的安全执行
+
+    参数:
+        func (function): 被装饰的函数
+
+    返回:
+        function: 返回带锁机制的包装函数
+    """
     def with_locking(*args, **kwargs):
+        """
+        带有线程锁的包装函数
+
+        参数:
+            *args: 函数的位置参数
+            **kwargs: 函数的关键字参数
+
+        返回:
+            any: 函数执行结果
+        """
+        # 使用全局锁确保同一时间只有一个线程执行该函数
         with _lock:
             return func(*args, **kwargs)
 
     return with_locking
+
