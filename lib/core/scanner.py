c96e8e3db3262f31b9916b45ef02978dd2cd4ad1
codervibe <3217277135@qq.com>
2025-11-21 01:09:03 +0800

    chore(i18n): 将扫描器模块中的注释和日志信息翻译为中文- 将GPL许可证声明翻译为中文 - 将方法注释和文档字符串翻译为中文 - 将调试日志消息翻译为中文- 更新上下文默认值的中文描述 - 翻译通配符重定向检测相关的注释说明 - 将静态方法generate_redirect_regex的文档翻译为中文并优化表述

--- lib/core/scanner.py
+++ lib/core/scanner.py
@@ -1,20 +1,17 @@
 # -*- coding: utf-8 -*-
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
+#  本程序是自由软件；您可以重新分发它和/或修改它
+#  遵循自由软件基金会发布的GNU通用公共许可证的条款；
+#  许可证的版本2，或（根据您的选择）任何更高版本。
 #
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+#  本程序的分发是希望它有用，
+#  但没有任何担保；甚至没有适销性或特定用途适用性的暗示保证。
+#  有关详细信息，请参阅GNU通用公共许可证。
 #
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  您应该已经收到GNU通用公共许可证的副本；
+#  如果没有，请写信给自由软件基金会，地址：51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 #
-#  Author: Mauro Soria
+#  作者: Mauro Soria
 
 import re
 
@@ -35,7 +32,7 @@ class Scanner:
     def __init__(self, requester, **kwargs):
         self.path = kwargs.get("path", "")
         self.tested = kwargs.get("tested", [])
-        self.context = kwargs.get("context", "all cases")
+        self.context = kwargs.get("context", "所有情况")
         self.requester = requester
         self.response = None
         self.wildcard_redirect_regex = None
@@ -43,8 +40,7 @@ class Scanner:
 
     def setup(self):
         """
-        Generate wildcard response information containers, this will be
-        used to compare with other path responses
+        生成通配符响应信息容器，这将用于与其他路径响应进行比较
         """
 
         first_path = self.path.replace(
@@ -55,11 +51,11 @@ class Scanner:
         self.response = first_response
 
         duplicate = self.get_duplicate(first_response)
-        # Another test was performed before and has the same response as this
+        # 之前已执行另一个测试并且响应与此相同
         if duplicate:
             self.content_parser = duplicate.content_parser
             self.wildcard_redirect_regex = duplicate.wildcard_redirect_regex
-            logger.debug(f'Skipped the second test for "{self.context}"')
+            logger.debug(f'跳过"{self.context}"的第二次测试')
             return
 
         second_path = self.path.replace(
@@ -75,7 +71,7 @@ class Scanner:
                 clean_path(second_response.redirect),
                 second_path,
             )
-            logger.debug(f'Pattern (regex) to detect wildcard redirects for "{self.context}": {self.wildcard_redirect_regex}')
+            logger.debug(f'用于检测"{self.context}"通配符重定向的模式（正则表达式）: {self.wildcard_redirect_regex}')
 
         self.content_parser = DynamicContentParser(
             first_response.content, second_response.content
@@ -90,9 +86,9 @@ class Scanner:
         return None
 
     def is_wildcard(self, response):
-        """Check if response is similar to wildcard response"""
+        """检查响应是否与通配符响应相似"""
 
-        # Compare 2 binary responses (Response.content is empty if the body is binary)
+        # 比较2个二进制响应（如果正文是二进制的，则Response.content为空）
         if not self.response.content and not response.content:
             return self.response.body == response.body
 
@@ -100,18 +96,18 @@ class Scanner:
 
     def check(self, path, response):
         """
-        Perform analyzing to see if the response is wildcard or not
+        执行分析以查看响应是否为通配符
         """
 
         if self.response.status != response.status:
             return True
 
-        # Read from line 129 to 138 to understand the workflow of this.
+        # 请阅读第129行到第138行以了解此工作流程。
         if self.wildcard_redirect_regex and response.redirect:
-            # - unquote(): Sometimes, some path characters get encoded or decoded in the response redirect
-            # but it's still a wildcard redirect, so unquote everything to prevent false positives
-            # - clean_path(): Get rid of queries and DOM in URL because of weird behaviours could happen
-            # with them, so messy that I give up on finding a way to test them
+            # - unquote(): 有时，响应重定向中的一些路径字符会被编码或解码
+            # 但它仍然是通配符重定向，所以取消引用所有内容以防止误报
+            # - clean_path(): 去除URL中的查询和DOM，因为它们可能发生奇怪的行为
+            # 太混乱了，我放弃了寻找测试它们的方法
             path = unquote(clean_path(path))
             redirect = unquote(clean_path(response.redirect))
             regex_to_compare = self.wildcard_redirect_regex.replace(
@@ -119,9 +115,9 @@ class Scanner:
             )
             is_wildcard_redirect = re.match(regex_to_compare, redirect, re.IGNORECASE)
 
-            # If redirection doesn't match the rule, mark as found
+            # 如果重定向不匹配规则，则标记为已找到
             if not is_wildcard_redirect:
-                logger.debug(f'"{redirect}" doesn\'t match the regular expression "{regex_to_compare}", passing')
+                logger.debug(f'"{redirect}"不匹配正则表达式"{regex_to_compare}"，通过')
                 return True
 
         if self.is_wildcard(response):
@@ -132,16 +128,15 @@ class Scanner:
     @staticmethod
     def generate_redirect_regex(first_loc, first_path, second_loc, second_path):
         """
-        From 2 redirects of wildcard responses, generate a regexp that matches
-        every wildcard redirect.
+        从2个通配符响应的重定向中，生成一个匹配每个通配符重定向的正则表达式。
 
-        How it works:
-        1. Replace path in 2 redirect URLs (if it gets reflected in) with a mark
-           (e.g. /path1 -> /foo/path1 and /path2 -> /foo/path2 will become /foo/[mark] for both)
-        2. Compare 2 redirects and generate a regex that matches both
-           (e.g. /foo/[mark]?a=1 and /foo/[mark]?a=2 will have the regex: ^/foo/[mark]?a=(.*)$)
-        3. Next time if it redirects, replace mark in regex with the path and check if it matches
-           (e.g. /path3 -> /foo/path3?a=5, the regex becomes ^/foo/path3?a=(.*)$, which matches)
+        工作原理：
+        1. 用标记替换2个重定向URL中的路径（如果它被反射出来）
+           （例如 /path1 -> /foo/path1 和 /path2 -> /foo/path2 都会变成 /foo/[mark]）
+        2. 比较2个重定向并生成匹配两者的正则表达式
+           （例如 /foo/[mark]?a=1 和 /foo/[mark]?a=2 将有正则表达式: ^/foo/[mark]?a=(.*)$）
+        3. 下次重定向时，用路径替换正则表达式中的标记并检查是否匹配
+           （例如 /path3 -> /foo/path3?a=5，正则表达式变成 ^/foo/path3?a=(.*)$，匹配成功）
         """
 
         if first_path:
