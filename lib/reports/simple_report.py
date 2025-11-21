5bcca8c44902a30f8dc8a7ee953c9e80bd8bc982
codervibe <3217277135@qq.com>
2025-11-21 11:05:46 +0800

    docs(reports):为SimpleReport类添加中文文档字符串
    - 为SimpleReport类添加了详细的类级别文档字符串
    -为generate方法添加了完整的函数文档字符串- 添加了参数和返回值的详细说明
    - 包含了方法功能的具体描述和实现细节说明

--- lib/reports/simple_report.py
+++ lib/reports/simple_report.py
@@ -1,25 +1,26 @@
-# -*- coding: utf-8 -*-
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#
-#  Author: Mauro Soria
-
 from lib.core.settings import NEW_LINE
 from lib.reports.base import FileBaseReport
 
 
 class SimpleReport(FileBaseReport):
+    """
+    简单报告生成器类
+
+    继承自FileBaseReport基类，用于生成简单的URL报告
+    """
+
     def generate(self, entries):
+        """
+        生成简单格式的报告
+
+        将输入的条目列表转换为以换行符分隔的URL字符串报告
+
+        参数:
+            entries: 包含条目对象的可迭代对象，每个条目对象需要有url属性
+
+        返回:
+            str: 由换行符连接的所有条目URL组成的字符串报告
+        """
+        # 使用生成器表达式提取所有条目的URL，并用换行符连接成一个字符串
         return NEW_LINE.join(entry.url for entry in entries)
+
