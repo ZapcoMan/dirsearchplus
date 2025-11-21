c05e52238a69306f1285a36ec8ee2c7f9f18ed6d
codervibe <3217277135@qq.com>
2025-11-21 11:06:50 +0800

    refactor(utils):优化随机字符串生成功能
    - 移除文件头部的旧许可证注释
    - 添加函数文档字符串，明确参数和返回值说明
    - 重构字符集定义和排除逻辑，提高可读性- 简化随机字符串生成方式- 增强代码注释以提升维护性

--- lib/utils/random.py
+++ lib/utils/random.py
@@ -1,29 +1,25 @@
-# -*- coding: utf-8 -*-
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#
-#  Author: Mauro Soria
-
 import random
 import string
 
 
 def rand_string(n, omit=None):
+    """
+    生成指定长度的随机字符串
+
+    参数:
+        n (int): 要生成的随机字符串的长度
+        omit (str, optional): 需要从字符集中排除的字符集合，默认为None表示不排除任何字符
+
+    返回:
+        str: 生成的随机字符串
+    """
+    # 定义字符集，包含大小写字母和数字
     seq = string.ascii_lowercase + string.ascii_uppercase + string.digits
 
+    # 如果指定了要排除的字符，则从字符集中移除这些字符
     if omit:
         seq = list(set(seq) - set(omit))
 
+    # 从字符集中随机选择n个字符并拼接成字符串
     return "".join(random.choice(seq) for _ in range(n))
+
