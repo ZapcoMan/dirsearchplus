c40f90263b4e27d8f5d7cf66d8643f886912412e
codervibe <3217277135@qq.com>
2025-11-21 11:11:51 +0800

    refactor(reports):重构文件基础报告类
    - 移除文件头部的GPL许可证注释
    - 添加类和方法的中文文档字符串
    - 规范化Windows系统下的文件路径
    - 实现条目保存到文件的功能
    - 抽象generate方法供子类实现- 添加输入参数和返回值类型注释

--- lib/reports/base.py
+++ lib/reports/base.py
@@ -1,27 +1,17 @@
-# -*- coding: utf-8 -*-
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#
-#  Author: Mauro Soria
-
 from lib.core.decorators import locked
 from lib.core.settings import IS_WINDOWS
 
 
 class FileBaseReport:
+    """
+    文件基础报告类，用于生成和保存报告到文件
+
+    Args:
+        output_file (str): 输出文件路径
+    """
+
     def __init__(self, output_file):
+        # 如果是Windows系统，规范化文件路径
         if IS_WINDOWS:
             from os.path import normpath
 
@@ -31,12 +21,36 @@ class FileBaseReport:
 
     @locked
     def save(self, entries):
+        """
+        保存条目到输出文件
+
+        Args:
+            entries (list): 要保存的条目列表
+
+        Returns:
+            None
+        """
+        # 如果没有条目则直接返回
         if not entries:
             return
 
+        # 打开文件并写入生成的内容
         with open(self.output_file, "w") as fd:
             fd.writelines(self.generate(entries))
             fd.flush()
 
     def generate(self, entries):
+        """
+        生成报告内容的抽象方法，需要子类实现
+
+        Args:
+            entries (list): 要生成报告的条目列表
+
+        Returns:
+            str: 生成的报告内容
+
+        Raises:
+            NotImplementedError: 当子类没有实现此方法时抛出
+        """
         raise NotImplementedError
+
